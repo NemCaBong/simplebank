4de3dbce3b73a91f5c45796d8d53ddaf2c09e361
NemCaBong <89348778+NemCaBong@users.noreply.github.com>
2023-06-07 12:00:34 +0700

    first commit

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,100 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/techschool/simplebank/db/util"
+)
+
+// when doing test
+// each test should create another account
+// make sure they are independent from each other
+// very hard to maintain to have 100 of tests depend on each other
+// not let the test afftect the result of the other one.
+func createRandomAccount(t *testing.T) Account {
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	// using testify lib to test for err must be nil
+	// fail the test if not nil
+	require.NoError(t, err)
+	// make sure the account is not empty
+	require.NotEmpty(t, account)
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+	// check the auto generated field is not zero
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+	return account
+}
+
+// if have Test prefix in the name, we can run test on that func.
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: util.RandomMoney(),
+	}
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, arg.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	// try to get row of the record that we have deleted and check if it's error
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	// make sure it's error and have no record in account2
+	require.Error(t, err)
+	require.Empty(t, account2)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+	// return 5 records a time
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:thobeogalaxy257@localhost:5432/simple_bank?sslmode=disable"
+)
+
+// main entry point for all testing
+// inside 1 package -> in this are db
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to the DB:", err)
+	}
+	// create a new &Queries struct
+	testQueries = New(conn)
+
+	// start running the unit test
+	// return the exit code whether test pass or fail
+	// report the code to the os.Exit()
+	os.Exit(m.Run())
+}
